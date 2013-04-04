bf25cb253de70f2d8de43dcee995253231df47cf
Austin Flores <ctrl4ltdeleteme@gmail.com>
2013-04-04 18:36:35 +0200

    set up baby litttle server thing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   This is a small mini http server and socket.io server.  It does two things:
   1. Maintain an open connection with listening client sockets
   2. Respond to http requests and pass their json to listening socket clients
-     curl -XPOST 'http://streamer.dev:8081/runs/5' -H "Content-Type: application/json" -d '{"id":"5","important_values": "stuff"}'
+     curl -XPOST 'http://streamer.dev:8081/foot/3' -H "Content-Type: application/json" -d '{"id":"3","foot_updates": "stuff"}'
 
 */
 require.main.paths.push('/usr/local/lib/node_modules');
@@ -22,13 +22,13 @@ app.configure(function(){
 });
 
 
-app.post(/runs\/(\d+)/, function(req, res){
+app.post(/([^\/]+)\/(\d+)/, function(req, res){
   if(req.ip !== '127.0.0.1'){
     res.send("ERROR: Invalid Source.\n");
     return;
   }
   var news = req.body;
-  news.room = 'run_' + req.params[0];
+  news.room = req.params[0] + '_' + req.params[1];
   io_client.emit('rebroadcast', news );
   res.send("SUCCESS: Streaming to other clients.\n");
 });
@@ -41,6 +41,9 @@ app.listen(8081);
 
 io.sockets.on('connection', function (client) {
   console.log('Connected to streamer');
+
+  client.emit('declare_room', {});
+
   client.on('subscribe', function(data) { client.join(data.room); });
 
   client.on('unsubscribe', function(data) { client.leave(data.room); });
